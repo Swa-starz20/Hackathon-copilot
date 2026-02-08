3e6fc6d47737e0af10b860af1efe781f8e0cac8f
Swanand Buchake <swanandbuchake@gmail.com>
2026-02-08 23:54:43 +0530

    Remove unused config options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */ 
 };
-
 export default nextConfig;
